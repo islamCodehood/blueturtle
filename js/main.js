a92a9934292b851e8e0522dab074b30109fa16cf
Islam <islam.sayed8@gmail.com>
2019-02-20 11:42:29 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    const moon = document.querySelector('.moon')
+    const sun = document.querySelector('.sun')
+    const switchArea = document.getElementsByClassName('switch-area')
+    const wrapper = document.querySelector('.wrapper')
+    const articlesWrappers = document.getElementsByClassName('article-wrapper')
+    Array.prototype.forEach.call(switchArea, function(area) {
+        area.addEventListener('click', function(e) {
+            console.log(e.target)
+            if (e.target.parentElement.parentElement === sun ||e.target === sun) {
+                console.log()
+                sun.classList.toggle('hidden')
+                moon.classList.toggle('hidden')
+                document.body.classList.add('light-theme-body')
+                wrapper.classList.add('light-theme-wrapper')
+                Array.prototype.forEach.call(articlesWrappers, function(articleWrapper) {
+                    articleWrapper.classList.add('light-theme-article-wrapper')
+                })
+            } else if (e.target.parentElement.parentElement === moon || e.target === moon) {
+                sun.classList.toggle('hidden')
+                moon.classList.toggle('hidden')
+                document.body.classList.remove('light-theme-body')
+                wrapper.classList.remove('light-theme-wrapper')
+                Array.prototype.forEach.call(articlesWrappers, function(articleWrapper) {
+                    articleWrapper.classList.remove('light-theme-article-wrapper')
+                })
+            }
+        })
+    })
+    
+    
+}
